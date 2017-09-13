3f5f1ba59891e5475ee2b9ebb806800f48bae6f6
Crasicus <matticus10@hotmail.com>
2017-09-13 09:29:34 +0100

    Initial Upload

---
+++ 2017/Kings BYOD Helpdesk/Kings BYOD Helpdesk/PupilReport.aspx.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Kings_BYOD_Helpdesk
+{
+    public partial class PupilReport : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                string query;
+
+                //query = "SELECT b.Expr1, a.schoolUsername, a.dateLogged FROM deviceRegistration AS a INNER JOIN (SELECT COUNT(*) AS Expr1, schoolUsername FROM deviceRegistration WHERE (schoolUsername = '"+ Session["pupilSearch"] +"') GROUP BY schoolUsername) AS b ON a.schoolUsername = b.schoolUsername";
+                query = "SELECT ticketID, deviceType, OSType, dateLogged FROM deviceRegistration WHERE (schoolUsername = '" + Session["pupilSearch"] + "')";
+
+                if (Session["pupilSearch"] != null)
+                {
+                    StringBuilder status = new StringBuilder();
+                    status.Append("<h3>Username: " + Session["pupilSearch"] + "</h3>");
+                    status.Append("<h3>Total Tickets: " + recordCount(query) + "</h3>");
+                    PlaceHolder1.Controls.Add(new Literal { Text = status.ToString() });
+                    if (recordCount(query) > 0)
+                    {
+                        PlaceHolder1.Controls.Add(new Literal { Text = pupilSearchTable(query) });
+                    }
+                    Session["pupilSearch"] = null;
+                }
+            }
+        }
+        //Row count
+        private int recordCount(string query)
+        {
+            DataTable dt = this.GetDataPS(query);
+
+            int i = 0;
+            foreach (DataRow row in dt.Rows)
+            {
+                i++;
+            }
+            return i;
+        }
+        //Create each table based on passed query
+        private string pupilSearchTable(string query)
+        {
+            //Populating a DataTable from database.
+            DataTable dt = this.GetDataPS(query);
+
+            //Building an HTML string.
+            StringBuilder html = new StringBuilder();
+
+            //Table start.
+            html.Append("<table border = '1' cellpadding='5'>");
+
+            //Building the Header row.
+            html.Append("<tr>");
+            foreach (DataColumn column in dt.Columns)
+            {
+                html.Append("<th>");
+                html.Append(column.ColumnName);
+                html.Append("</th>");
+            }
+            html.Append("</tr>");
+
+            //Building the Data rows.
+            foreach (DataRow row in dt.Rows)
+            {
+                html.Append("<tr>");
+                foreach (DataColumn column in dt.Columns)
+                {
+                    html.Append("<td>");
+                    html.Append(row[column.ColumnName]);
+                    html.Append("</td>");
+                }
+                html.Append("</tr>");
+            }
+
+            //Table end.
+            html.Append("</table>");
+
+            return html.ToString();
+        }
+        private DataTable GetDataPS(string query)
+        {
+            string SQLConnection = ConfigurationManager.ConnectionStrings["SQLConnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(SQLConnection))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            return dt;
+                        }
+                    }
+                }
+            }
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Session["pupilSearch"] = TextBox1.Text;
+            Response.Redirect(Request.RawUrl);
+        }
+    }
+}
---
+++ 2017/Kings BYOD Helpdesk/Kings BYOD Helpdesk/Ticket.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Kings_BYOD_Helpdesk
+{
+    public partial class Ticket : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                string ticketID = Request.QueryString["ticketid"]; //read phone from querystring.
+
+                string query;
+
+                query = "SELECT schoolUsername AS [School Username], schoolPassword AS [School Password], deviceUsername AS [Device Username], devicePassword AS [Device Password] FROM deviceRegistration WHERE ticketID = " + ticketID;
+
+                PlaceHolder1.Controls.Add(new Literal { Text = statusTable(query) });
+                
+            }
+        }
+        //Row count
+        private int recordCount(string query)
+        {
+            DataTable dt = this.GetData(query);
+
+            int i = 0;
+            foreach (DataRow row in dt.Rows)
+            {
+                i++;
+            }
+            return i;
+        }
+        //Create each table based on passed query
+        private string statusTable(string query)
+        {
+            //Populating a DataTable from database.
+            DataTable dt = this.GetData(query);
+
+            //Building an HTML string.
+            StringBuilder html = new StringBuilder();
+
+            //Table start.
+            html.Append("<table border = '1' cellpadding='5'>");
+
+            //Building the Header row.
+            html.Append("<tr>");
+            foreach (DataColumn column in dt.Columns)
+            {
+                html.Append("<th>");
+                html.Append(column.ColumnName);
+                html.Append("</th>");
+            }
+            html.Append("</tr>");
+
+            //Building the Data rows.
+            foreach (DataRow row in dt.Rows)
+            {
+                html.Append("<tr>");
+                foreach (DataColumn column in dt.Columns)
+                {
+                    html.Append("<td>");
+                    html.Append(row[column.ColumnName]);
+                    html.Append("</td>");
+                }
+                html.Append("</tr>");
+            }
+
+            //Table end.
+            html.Append("</table>");
+
+            return html.ToString();
+        }
+        private DataTable GetData(string query)
+        {
+            string SQLConnection = ConfigurationManager.ConnectionStrings["SQLConnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(SQLConnection))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            return dt;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
